e1753fbcd10b1a16e2a128d719bce18d5c196b6d
OCitkowski <o.citkowski@gmail.com>
2022-08-11 11:12:08 +0300

    add crypt

--- inst_bot/auth_data.py
+++ inst_bot/auth_data.py
@@ -1,2 +1,12 @@
-user_name = ''
-password = ''
+user_name = 'odinbogus'
+password = '3dZxtd5cjt'
+path = '/home/fox/PycharmProjects/python_parsing/inst_bot/chromedriver'
+site_path = 'https://www.instagram.com/'
+
+
+if __name__ == "__main__":
+    key_path = '../cr_graphy/new'
+    create_key(key_path)
+    # key = load_key(key_path)
+    key = Fernet.generate_key()
+    encrypt_file('../cr_graphy/test.txt', key)
--- inst_bot/main.py
+++ inst_bot/main.py
@@ -1,35 +1,38 @@
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
-from auth_data import password, user_name
+from auth_data import password, user_name, path, site_path
 import time, random
 
-def login(password, user_name):
+def login():
 
-    browser = webdriver.Chrome('/home/fox/PycharmProjects/python_parsing/inst_bot/chromedriver')
-
-    browser.get('https://www.instagram.com/')
+    browser = webdriver.Chrome(path)
+    browser.get(site_path)
     time.sleep(random.randrange( 3, 5))
 
-    username_input = browser.find_element('name', 'username')
-    username_input.clear()
-    username_input.send_keys('xdfgjfgdhj')
+    try:
+        username_input = browser.find_element('name', 'username')
+        username_input.clear()
+        username_input.send_keys(user_name)
 
-    time.sleep(random.randrange(3, 5))
+        time.sleep(random.randrange(3, 5))
 
-    password_input = browser.find_element('name', 'password')
-    password_input.clear()
-    password_input.send_keys('xdfgjfgdhj')
+        password_input = browser.find_element('name', 'password')
+        password_input.clear()
+        password_input.send_keys(password)
 
-    time.sleep(random.randrange(3, 5))
+        time.sleep(random.randrange(3, 5))
 
-    password_input.send_keys(Keys.ENTER)
+        password_input.send_keys(Keys.ENTER)
 
-    username_input = browser.find_element('name', 'username')
-    username_input.clear()
+        time.sleep(random.randrange(30, 50))
+        browser.close()
+        browser.quit()
+
+    except Exception as ex:
+        print(ex)
+        browser.close()
+        browser.quit()
 
 
-    time.sleep(random.randrange(3, 5))
-    browser.close()
-    browser.quit()
-
-login(password, user_name)
+if __name__ == '__main__':
+    login()
