022d0b1364438eb2882cd137d7c93fff61789c6c
fox <o.citkowski@gmail.com>
2023-03-26 12:39:23 +0200

    26.03.2023 +1

--- scrapy/dict_com/dict_com/pipelines.py
+++ scrapy/dict_com/dict_com/pipelines.py
@@ -10,6 +10,18 @@ import json
 import logging
 
 
+def save_data_in_json_file(self, data):
+    result = False
+    try:
+        with open(self._json_file_name + '.json', 'w') as write_file:
+            json.dump(data, write_file, ensure_ascii=False)
+            result = True
+        print(f'{self._json_file_name}.json save to root')
+    except:
+        print(f'{self._json_file_name}.json don`t save to root')
+    return result
+
+
 class DictComPipeline:
     def process_item(self, item, spider):
         return item
@@ -23,36 +35,48 @@ class JsonWriterPipeline(object):
         self.file.close()
 
     def process_item(self, item, spider):
-        # збереження даних у JSON форматі
-        try:
-            data = {
-                "****": 'tttt',
-                "word": item["word"],
-                "translation": item["translation"],
-                "morphology": item["morphology"]
-            }
-            line = json.dumps(data) + "\n"
-            logging.info(f"line: -{line}")
+        # зчитування шаблонного файлу
+        with open("data.json", "r") as filedata:
+            data = filedata.read()
 
-        except Exception as e:
+        data.append(item)
+        # # запис значень з item в template
+        # data["word"] = item["word"]
+        # data["translation"] = item["translation"]
+        # data["part_of_speech"] = item["part_of_speech"]
+        # data["german_alternatives"] = item["german_alternatives"]
+        # # ...
 
-            logging.error(f"Error, json dumped : {e}")
+        # запис template у вихідний файл
+        with open("data.json", "w") as filewrite:
+            # json.dump(data, f, ensure_ascii=False, indent=4)
+            filewrite.write(json.dumps(data, indent=4))
 
-
-
-        # заміна значення "translation" у шаблонному файлі
-        with open("template.json") as f:
-            template = json.load(f)
-        template["translation"] = item["translation"]
-        with open("output.json", "w") as f:
-            json.dump(template, f, indent=4)
-
-        # запис даних у вихідний файл
-        self.file.write(line)
         return item
 
-    def process_item(self, item, spider):
-        line = json.dumps(dict(item)) + "\n"
-        self.file.write(line)
-        logging.info(f"Processed item: {item['word']}")
+    def _process_item(self, item, spider):
+        # Відкрийте файл та завантажте його в змінну data
+        with open('deck.json', 'r') as f:
+            data = json.load(f)
+        i = 0
+        # Перезаписати значення
+        for note in data['notes']:
+            fields = note['fields']
+            i += 1
+
+            if fields[0] == item["word"]:
+                for i, field in enumerate(fields):
+
+                    if i == 2:
+                        fields[i] = item["translation"]
+                    elif i == 7:
+                        fields[i] = item["german_alternatives"]
+                logging.debug(f"for: {i} -- {item['word']}| Ukr - {fields[2]} | german_alternatives - {fields[7]} ")
+
+            # print(i, fields)
+
+        # Зберегти нові дані у файл
+        with open('deck.json', 'w') as f:
+            json.dump(data, f)
+
         return item
--- scrapy/dict_com/dict_com/settings.py
+++ scrapy/dict_com/dict_com/settings.py
@@ -24,7 +24,7 @@ ROBOTSTXT_OBEY = False
 # Configure a delay for requests for the same website (default: 0)
 # See https://docs.scrapy.org/en/latest/topics/settings.html#download-delay
 # See also autothrottle settings and docs
-DOWNLOAD_DELAY = 1.5
+DOWNLOAD_DELAY = 3
 # The download delay setting will honor only one of:
 # CONCURRENT_REQUESTS_PER_DOMAIN = 16
 # CONCURRENT_REQUESTS_PER_IP = 16
@@ -101,3 +101,11 @@ LOG_FORMAT = '%(asctime)s [%(name)s] %(levelname)s: %(message)s'
 ITEM_PIPELINES = {
     'dict_com.pipelines.JsonWriterPipeline': 800,  # look to pipelines.py
 }
+
+
+# В даному коді вказується, що Scrapy повинен використовувати браузерний агент, який схожий на Google Chrome, і не
+# враховувати robots.txt. Також включено обробку кукісів, що дозволить зберігати дані авторизації.
+USER_AGENT = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3'
+ROBOTSTXT_OBEY = False
+COOKIES_ENABLED = True
+DOWNLOAD_DELAY = 1
